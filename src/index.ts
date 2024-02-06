b962f3ccbacca875ec4e75e66a13edebd36eb18c
Your Name <janengene12@gmail.com>
2024-02-06 13:34:37 +0300

    login,signup,landing page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+let signUpBtn = document.getElementById("sign-up") as HTMLButtonElement;
+let logInBtn = document.getElementById("login") as HTMLButtonElement;
+
+signUpBtn.addEventListener("click", () => {
+    window.location.href = 'Signup.html'
+})
+logInBtn.addEventListener("click", () => {
+    window.location.href = 'login.html'
+})
+
+
+
 
